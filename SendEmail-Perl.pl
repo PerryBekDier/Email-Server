cf46839aefe50263d1c69c8f721ce6f27e56dc91
Bob Stuart <bobstuart@notsofruity.com>
2015-05-05 11:16:21 -0100

    Script Updated

--- SendEmail-Perl.pl
+++ SendEmail-Perl.pl
@@ -5,3 +5,26 @@ use Email::Sender::Simple qw(sendmail);
 use Email::Sender::Transport::SMTP ();
 use Email::Simple ();
 use Email::Simple::Creator ();
+
+my $smtpserver = 'Server_IP_here';
+my $smtpport = 25;
+my $smtpuser   = 'bob';
+my $smtppassword = 'Summer15';
+
+my $transport = Email::Sender::Transport::SMTP->new({
+  host => $smtpserver,
+  port => $smtpport,
+  sasl_username => $smtpuser,
+  sasl_password => $smtppassword,
+});
+
+my $email = Email::Simple->create(
+  header => [
+    To      => 'mymail@gmail.com',
+    From    => 'sender@example.com',
+    Subject => 'Hi!',
+  ],
+  body => "This is my message\n",
+);
+
+sendmail($email, { transport => $transport });
